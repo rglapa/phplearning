6d81fbe9f27f38420a1324532911f45276168493
rglapa <rubenglapa@gmail.com>
2025-09-30 09:49:31 -0700

    Update Sep 30

--- public/index.php
+++ public/index.php
@@ -1,11 +1,16 @@
 <?php
-require __DIR__ . '/layout/endheading.php';
-require __DIR__ . '/layout/footer.php';
-require __DIR__ . '/layout/heading.php';
+require_once __DIR__ . '/layout/endheading.php';
+require_once __DIR__ . '/layout/footer.php';
+require_once __DIR__ . '/layout/heading.php';
 require_once __DIR__ . '/router.php';
 
 get('/', 'views/index.view.php');
 get('/AddClass', 'jquery_learning/AddClass.php');
+get('/Ajax', 'jquery_learning/Ajax.php');
+get('/Appending', 'jquery_learning/Appending.php');
+get('/Chaining', 'jquery_learning/Chaining.php');
+get('/filtering', 'jquery_learning/Filtering.php');
+get('/Appending', 'jquery_learning/Appending.php');
 get('/ChangeClass', 'jquery_learning/ChangeClass.php');
 get('/RemoveClass', 'jquery_learning/RemoveClass.php');
 get('/ToggleClass', 'jquery_learning/ToggleClass.php');
